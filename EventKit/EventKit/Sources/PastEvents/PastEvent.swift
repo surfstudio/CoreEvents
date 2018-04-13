4c32e43725f28679baf7016328ed7db988d86edc
Alexander Kravchenkov <akravchenkov@surfstudio.co>
2018-04-13 12:31:55 +0300

    Added past event and tests

---
+++ EventKit/EventKit/Sources/PastEvents/PastEvent.swift
@@ -0,0 +1,56 @@
+//
+//  PastEvent.swift
+//  EventKit
+//
+//  Created by Alexander Kravchenkov on 13.04.2018.
+//  Copyright © 2018 Alexander Kravchenkov. All rights reserved.
+//
+
+/// Present event is like Past time in English.
+/// This event emits **all old emited value and all new messages**.
+/// Provide `+=` operation for adding new listners: `let evet += { value in ... }`
+/// `Input` - it's a type of value this event will emit.
+class PastEvent<Input>: Event {
+
+    public typealias Lambda = (Input) -> Void
+
+    private var listners: [Lambda]
+    private var laterEmits: [Input]
+
+    public init() {
+        self.laterEmits = [Input]()
+        self.listners = []
+    }
+
+    /// Add new listner and emits last emited message only for this listner.
+    ///
+    /// - Parameter listner: New listner.
+    public func addListner(_ listner: @escaping Lambda) {
+        if !self.laterEmits.isEmpty {
+            self.laterEmits.forEach { listner($0) }
+        }
+        self.listners.append(listner)
+    }
+
+    /// Notify all listners.
+    ///
+    /// - Parameter input: Data for listners.
+    public func invoke(with input: Input) {
+        self.laterEmits.append(input)
+        self.listners.forEach({ $0(input) })
+    }
+
+    /// Remove all listners and erase last emited value
+    public func clear() {
+        self.laterEmits.removeAll()
+        self.listners.removeAll()
+    }
+}
+
+// MARK: - Operations
+
+extension PastEvent {
+    public static func += (left: PastEvent<Input>, right: @escaping Lambda) {
+        left.addListner(right)
+    }
+}
---
+++ EventKit/EventKitTests/PastEventTests/PastEventTests.swift
@@ -0,0 +1,173 @@
+//
+//  PastEventTests.swift
+//  EventKitTests
+//
+//  Created by Alexander Kravchenkov on 13.04.2018.
+//  Copyright © 2018 Alexander Kravchenkov. All rights reserved.
+//
+
+import XCTest
+@testable import EventKit
+
+class PastEventTests: XCTestCase {
+
+    // MARK: - Nested types
+
+    private struct EventContainer {
+        var event = PastEvent<String>()
+
+        func emit(value: String) {
+            event.invoke(with: value)
+        }
+    }
+
+    // MARK: - Lifecycle
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    // MARK: - Positive tests
+
+    func testTahatEventEmitValue() {
+
+        // Arrange
+
+        let emiter = EventContainer()
+        let message = "Hello world"
+
+        // Act
+
+        var emited: String?
+
+        emiter.event += { value in
+            emited = value
+        }
+
+        emiter.emit(value: message)
+
+        // Assert
+
+        XCTAssertEqual(message, emited)
+    }
+
+    func testThatEventCanEmitMulticast() {
+        // Arrange
+
+        let emiter = EventContainer()
+        let message = "Hello world"
+        let listenersCount = 10
+
+        // Act
+
+        var emited = [String]()
+
+        for _ in 0..<listenersCount {
+            emiter.event += { value in
+                emited.append(value)
+            }
+        }
+
+        emiter.emit(value: message)
+
+        // Assert
+
+        XCTAssertEqual(emited.count, listenersCount)
+        emited.forEach { XCTAssertEqual($0, message) }
+    }
+
+    func testThatEventEmitsManyTimes() {
+        // Arrange
+
+        let emiter = EventContainer()
+        let message = "Hello world"
+        let emitCount = 10
+
+        // Act
+
+        var emited = [String]()
+
+        emiter.event += { value in
+            emited.append(value)
+        }
+
+        for _ in 0..<emitCount {
+            emiter.emit(value: message)
+        }
+
+        // Assert
+
+        XCTAssertEqual(emited.count, emitCount)
+        emited.forEach { XCTAssertEqual($0, message) }
+    }
+
+    func testThatEventEmitsAllLastEmitedMessage() {
+        // Arrange
+
+        let emiter = EventContainer()
+        let firstMessage = "Hello world"
+        let secondMessage = "Hello (:"
+        let thirdMessage = "How are you?"
+
+        // Act
+
+        var allEmited = [String]()
+
+        emiter.event += { value in
+            allEmited.append(value)
+        }
+
+        emiter.emit(value: firstMessage)
+        emiter.emit(value: secondMessage)
+
+        var newEmited = [String]()
+
+        emiter.event += { value in
+            newEmited.append(value)
+        }
+
+        let afterSubscribeMessageCount = newEmited.count
+
+        emiter.emit(value: thirdMessage)
+
+        // Assert
+
+        XCTAssertEqual(newEmited.count, allEmited.count)
+        XCTAssertEqual(afterSubscribeMessageCount, newEmited.count - 1)
+
+        for index in 0..<3 {
+            XCTAssertEqual(newEmited[index], allEmited[index])
+        }
+
+    }
+
+    func testThatClearMethodWorkSuccess() {
+        // Arrange
+
+        let emiter = EventContainer()
+        let firstMessage = "Hello world"
+        let lastMessage = "Hello (:"
+
+        // Act
+
+        var emited = [String]()
+
+        emiter.event += { value in
+            emited.append(value)
+        }
+
+        emiter.emit(value: firstMessage)
+
+        emiter.event.clear()
+
+        emiter.emit(value: lastMessage)
+
+        // Assert
+
+        XCTAssertEqual(emited.count, 1)
+    }
+}
